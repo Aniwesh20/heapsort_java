7879b71b91a2281447c44a0f8edcbbbaba1eafd9
Aniwesh20 <96642963+Aniwesh20@users.noreply.github.com>
2023-02-20 20:20:12 +0530

    Add files via upload

---
+++ Main (3).java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		
+		PriorityQueue<Integer>pq=new PriorityQueue<>();
+		
+		int []rank= {22,99,3,11,99,4,1};
+		
+		for(int val:rank){
+		    pq.add(val);
+		}
+		
+		while(pq.size()>0){
+		    System.out.println(pq.peek());
+		    pq.remove();
+		}
+	}
+}
+// HeapSort assending.
